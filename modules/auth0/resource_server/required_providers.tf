31ff460de6dcdde7175729de63719bccfe3ef318
Mark Tranter <mark.a.tranter@gmail.com>
2021-12-04 21:35:46 +1100

    Update required_providers.tf

--- modules/auth0/resource_server/required_providers.tf
+++ modules/auth0/resource_server/required_providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     auth0 = {
       source  = "alexkappa/auth0"
-      version = "~> 0.21.0"
+      version = ">= 0.21.0"
     }
   }
   required_version = ">= 1.0"
